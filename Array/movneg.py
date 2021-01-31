87b8ef183721c9ccfaf1f4f46be00d76c7524928
Manish Kumar <46210835+manishtu@users.noreply.github.com>
2021-01-31 19:27:10 +0530

    adding second method in Move all negative numbers to beginning

--- Array/movneg.py
+++ Array/movneg.py
@@ -11,5 +11,26 @@ for j in range(0,N):
 
 print(arr)
 
-# Method-2
+# Method -2
+# Two pointer Method
+arr = [-12, 11, -13, -5, 90,6, -7, 5, -3, -6]
+i=0
+j=len(arr)-1
+while(i<=j):
+    if arr[i]<0 and arr[j]<0:
+        i+=1
+    elif arr[i]<0 and arr[j]>0:
+        i+=1
+        j-=1
+    elif arr[i]>0 and arr[j]>0:
+        j -=1
+    else:
+        arr[i],arr[j] = arr[j],arr[i]
+        i+=1
+        j-=1
+print(arr)
+
+
+
+# Method-3
 # Arrange in incerasing order
