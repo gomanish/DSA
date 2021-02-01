22ce7934cf6cec073d51b3762b8cf8a6ddba6f83
Manish Kumar <46210835+manishtu@users.noreply.github.com>
2021-02-01 11:40:25 +0530

    Adding another Method for sorting array

--- Array/sorting.py
+++ Array/sorting.py
@@ -15,4 +15,42 @@ for i in arr:
         arr1.append(2)
 print(arr1)   
 
-# Method-2
+# Method-3
+arr = [0,2,1,2,0]
+n= len(arr)
+c0=0
+c1=0
+c2=0
+for i in range(n):
+    if arr[i]==0:
+        c0+=1
+    elif arr[i]==1:
+        c1+=1
+    else:
+        c2+=1
+for i in range(c0):
+    arr[i]=0
+for i in range(c0,c0+c1):
+    arr[i]=1
+for i in range(c0+c1,c0+c1+c2):
+    arr[i]=2
+print(arr)
+
+
+# Method-3
+#Dutch National flag algorithm
+arr = [0,2,1,2,0]
+low = 0
+mid = 0
+high = len(arr)-1
+while mid<=high:
+    if arr[mid]==0:
+        arr[low],arr[mid]=arr[mid],arr[low]
+        low+=1
+        mid+=1
+    elif arr[mid]==1:
+        mid+=1
+    else:
+        arr[mid],arr[high]=arr[high],arr[mid]
+        high-=1
+print(arr)
